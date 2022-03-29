2d632a184642f7a6eead4e86264284d74025b47f
Leostales <leostales@gmail.com>
2022-03-29 12:00:02 -0500

    SearchBar Update

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -20,9 +20,20 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
+  late List<WorldTime> filteredLocations;
+  bool typing = false;
+  TextEditingController _controller = new TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    filteredLocations = locations;
+    typing = false;
+  }
 
   void updateTime(index) async {
-    WorldTime instance = locations[index];
+    WorldTime instance = filteredLocations[index];
     await instance.getTime();
     // navigate to home screen
     Navigator.pop(context, {
@@ -33,30 +44,55 @@ class _ChooseLocationState extends State<ChooseLocation> {
     });
   }
 
+  void updateFiltered(String query) {
+    filteredLocations = [];
+    for (var country in locations) {
+    if (country.location.toLowerCase().contains(query.toLowerCase())) {
+      filteredLocations.add(country);
+    }
+    print("You did it!");
+    print(filteredLocations);
+    setState(() {
+
+    });
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
+        title: typing ? Container(
+          alignment: Alignment.centerLeft,
+          color: Colors.grey[200],
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Search',
+            ),
+            controller: _controller,
+            onChanged: (String text) {
+              updateFiltered(text);
+            },
+          ),
+        ) : Text("Choose a Location"),
         centerTitle: true,
         elevation: 0,
         actions: [
           IconButton(
-            onPressed: (){
-              showSearch(
-                context: context,
-                delegate: CustomSearchDelegate(),
-              );
+            icon: Icon(typing ? Icons.done : Icons.search),
+            onPressed: () {
+              setState(() {
+                typing = !typing;
+              });
             },
-            icon: const Icon(Icons.search),
           )
         ]
-
       ),
       body: ListView.builder(
-        itemCount: locations.length,
+        itemCount: filteredLocations.length,
         itemBuilder: (context, index){
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
@@ -65,9 +101,9 @@ class _ChooseLocationState extends State<ChooseLocation> {
                 onTap: () {
                   updateTime(index);
                 },
-                title: Text(locations[index].location),
+                title: Text(filteredLocations[index].location),
                 leading: CircleAvatar(
-                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  backgroundImage: AssetImage('assets/${filteredLocations[index].flag}'),
                 ),
               ),
             ),
@@ -76,104 +112,4 @@ class _ChooseLocationState extends State<ChooseLocation> {
       ),
     );
   }
-}
-
-
-class CustomSearchDelegate extends SearchDelegate {
-  List<WorldTime> searchTerms = [
-    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
-    WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
-    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
-    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
-    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
-    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
-    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
-    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
-  ];
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<WorldTime> matchQuery = [];
-    for (WorldTime country in searchTerms) {
-      if (country.location.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(country);
-      }
-    }
-    return ListView.builder(
-        itemCount: matchQuery.length,
-        itemBuilder: (context, index){
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-            child: Card(
-              child: ListTile(
-                onTap: () {
-                  query = matchQuery[index].location;
-                },
-                title: Text(matchQuery[index].location),
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage('assets/${matchQuery[index].flag}'),
-                ),
-              ),
-            ),
-          );
-        }
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<WorldTime> matchQuery = [];
-    for (var country in searchTerms) {
-      if (country.location.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(country);
-      }
-    }
-    return ListView.builder(
-        itemCount: matchQuery.length,
-        itemBuilder: (context, index){
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-            child: Card(
-              child: ListTile(
-                onTap: () {
-                  query = matchQuery[index].location;
-                  showResults(context);
-                  // updateTime(index);
-                },
-                title: Text(matchQuery[index].location),
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage('assets/${matchQuery[index].flag}'),
-                ),
-              ),
-            ),
-          );
-        }
-    );
-  }
-
-
-
-
 }
